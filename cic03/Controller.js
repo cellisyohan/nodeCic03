82f016bf459354b9d903d100a977aedb6e31d28b
my name <cellisyohan@gmail.com>
2022-08-16 13:17:13 -0300

    ok

--- cic03/Controller.js
+++ cic03/Controller.js
@@ -62,3 +62,102 @@ app.post('/cliente/:id/pedido', async(req,res)=>{
         });
     });
 });
+
+//listar todos os clientes
+
+app.get('/clientes', async(req, res)=>{
+    await cliente.findAll()
+    .then(ccli=>{
+        return res.json({
+            error:false,
+            ccli
+        });
+    }).catch(erro=>{
+        return res.status(400).json({
+            error:true,
+            message:" problema na conexão"
+        });
+    });
+});
+
+// listar pedido por clientes
+
+app.get('/clientes-pedidos', async(req, res)=>{
+    await cliente.findAll({include : [{all : true}]})
+      .then(pcli=>{
+        return res.json({
+            error:false,
+            pcli
+        });
+      }).catch(erro=>{
+        return res.status(400).json({
+            error:true,
+            message:" problema na na pesquisa"
+        });
+    });
+});
+
+// listar pedidos de UM cliente
+
+app.get('/clientes/:id/pedidos', async(req,res)=>{
+    await pedido.findAll({
+        where: {ClienteId:req.params.id}
+    }).then(pedidos=>{
+        return res.json({
+            error:false,
+            pedidos
+        });
+      }).catch(erro=>{
+        return res.status(400).json({
+            error:true,
+            message:" problema na conexão com a API."
+        });
+    });
+});
+
+//listar um pedido
+
+app.get('/pedido/:id', async(req,res)=>{
+    await pedido.findByPk (req.params.id)
+    .then(ped=>{
+        return res.json({
+            error:false,
+            ped
+        });
+      }).catch(erro=>{
+        return res.status(400).json({
+            error:true,
+            message:" problema na conexão com a API."
+        });
+    });
+});
+
+// fazer alteração no pedido
+
+app.put('/pedido/:id', async(req,res)=>{
+    const pedi={
+        id: req.params.id,
+        ClienteId: req.body.ClienteId,
+        data: req.body.data
+    }
+    if(! await cliente.findByPk(req.body.ClienteId)){
+        return res.status(400).json({
+            error: true,
+            message: "Cliente não existe."
+        }); 
+    };
+    await pedido.update(pedi,{
+        where: Sequelize.and({ClienteId : req.body.ClienteId})
+    }).then(umped=>{
+        return res.json({
+            error:false,
+            message :"Pedido atualizado com sucesso!",
+            umped
+        });
+      }).catch(erro=>{
+        return res.status(400).json({
+            error:true,
+            message:" problema na conexão com a API."
+        });
+    });
+});
