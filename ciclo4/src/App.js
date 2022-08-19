6cd9c4fb0798f60d69efdca08e29b120d1b48320
my name <cellisyohan@gmail.com>
2022-08-19 11:25:06 -0300

    aula4

---
+++ ciclo4/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Routes, Route } from'react-router-dom'
+import { Home } from './Home';
+import { Clientes } from './Cliente/Clientes';
+import { Menu } from './Menu';
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+      <Routes>
+        <Route path ='/' element={<Home/>}/>
+        <Route path ='/Listar-clientes' element={<Clientes/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
